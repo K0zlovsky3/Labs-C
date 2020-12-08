abfafba176baf7739c907fcc199f4bed1378182c
Konstantin Kozlov <38939201+K0zlovsky3@users.noreply.github.com>
2020-12-08 22:04:53 +0500

    Add files via upload

---
+++ Lab_10/Lab_10.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main()
+{
+    int mass[31], index = 0;
+    for (int num = 0; num <= 30; num++)
+    {
+        mass[num] = num + 1;
+        cout << "Index " << num << " = " << mass[num] << endl;
+    }
+
+    cout << "\n Enter the index you want to change: ";
+    cin >> index;
+    cout << endl;
+    mass[index] = 0;
+    for (int num = 0; num != 30; num++)
+    {
+        cout << "Index " << num << " = " << mass[num] << endl;
+    }
+    exit(0);
+    return 0;
+}
